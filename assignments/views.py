f89ecdd2e813a2321fb1e1e34dc0190d754bea68
Nitay Caspi <NitayCaspi12@gmail.com>
2023-08-22 03:41:19 +0300

    create views
    Created simple views for classrooms, courses, assignments, and submissions

--- assignments/views.py
+++ assignments/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Classroom, Course, Assignment, Submission
+from .serializers import ClassroomSerializer, CourseSerializer, AssignmentSerializer, SubmissionSerializer
+
+
+class ClassroomListCreateView(generics.ListCreateAPIView):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
+
+
+class CourseListCreateView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class AssignmentListCreateView(generics.ListCreateAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
+
+
+class SubmissionListCreateView(generics.ListCreateAPIView):
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionSerializer
