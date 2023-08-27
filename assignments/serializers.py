8497ee39070027b4828b10373225f9bd2393a999
Nitay Caspi <NitayCaspi12@gmail.com>
2023-08-27 15:19:28 +0300

    add user serializer

--- assignments/serializers.py
+++ assignments/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from .models import Classroom, Course, Assignment, Submission
+from .models import Classroom, Course, Assignment, Submission, CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
 
 class ClassroomSerializer(serializers.ModelSerializer):
