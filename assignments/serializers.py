ec468c267b70e2527188abdd61ec69b855024706
Nitay Caspi <NitayCaspi12@gmail.com>
2023-09-04 10:08:14 +0300

    fix password issue

--- assignments/serializers.py
+++ assignments/serializers.py
@@ -6,7 +6,19 @@ from .models import Classroom, Course, Assignment, Submission, CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class ClassroomSerializer(serializers.ModelSerializer):
