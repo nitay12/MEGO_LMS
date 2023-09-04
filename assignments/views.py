fd281d2f6c9ff639fbd58889e11a4d9ac26b6695
Nitay Caspi <NitayCaspi12@gmail.com>
2023-09-04 10:10:29 +0300

    add permissions to views

--- assignments/views.py
+++ assignments/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Course, Classroom, Assignment, Submission, CustomUser
+from .permissions import IsAdminOrCourseOwner, IsAdminOrUserOwner, IsAdminOrClassroomOwner, IsAdminOrExaminer, \
+    IsAdminOrExaminerOrAssignmentOwner
 from .serializers import CourseSerializer, ClassroomSerializer, AssignmentSerializer, SubmissionSerializer, \
     CustomUserSerializer
 
@@ -8,41 +11,75 @@ from .serializers import CourseSerializer, ClassroomSerializer, AssignmentSerial
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAdminOrUserOwner]
+    authentication_classes = [JWTAuthentication]
 
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAdminOrUserOwner]
+    authentication_classes = [JWTAuthentication]
 
 
 class CourseListCreateView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [IsAdminOrCourseOwner]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        if self.request.user.is_admin:
+            return Course.objects.all()
+        else:
+            return Course.objects.filter(classrooms__users=self.request.user)
 
 
 class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [IsAdminOrCourseOwner]
+    authentication_classes = [JWTAuthentication]
 
 
 class ClassroomListCreateView(generics.ListCreateAPIView):
     queryset = Classroom.objects.all()
     serializer_class = ClassroomSerializer
+    permission_classes = [IsAdminOrClassroomOwner]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        if self.request.user.is_admin:
+            return Classroom.objects.all()
+        else:
+            return Classroom.objects.filter(users=self.request.user)
 
 
 class ClassroomDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Classroom.objects.all()
     serializer_class = ClassroomSerializer
+    permission_classes = [IsAdminOrClassroomOwner]
+    authentication_classes = [JWTAuthentication]
 
 
 class AssignmentListCreateView(generics.ListCreateAPIView):
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
+    permission_classes = [IsAdminOrExaminer]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        if self.request.user.is_admin or self.request.user.is_staff:
+            return Assignment.objects.all()
+        else:
+            return Assignment.objects.filter(course__classrooms__users=self.request.user)
 
 
 class AssignmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
+    permission_classes = [IsAdminOrExaminerOrAssignmentOwner]
+    authentication_classes = [JWTAuthentication]
 
 
 class SubmissionListCreateView(generics.ListCreateAPIView):
