8ece5926f6b2ee1d7e7f0986345602b0b960e4ba
Nitay Caspi <NitayCaspi12@gmail.com>
2023-09-05 11:14:02 +0300

    add submission permissions

--- assignments/views.py
+++ assignments/views.py
@@ -1,9 +1,11 @@
+from django.utils import timezone
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Course, Classroom, Assignment, Submission, CustomUser
-from .permissions import IsAdminOrCourseOwner, IsAdminOrUserOwner, IsAdminOrClassroomOwner, IsAdminOrExaminer, \
-    IsAdminOrExaminerOrAssignmentOwner
+from .permissions import IsAdminOrCourseOwner, IsAdminOrUserOwner, IsAdminOrClassroomOwner, \
+    IsAdminOrExaminerOrAssignmentOwner, IsAdminOrExaminerOrSubmissionOwner
 from .serializers import CourseSerializer, ClassroomSerializer, AssignmentSerializer, SubmissionSerializer, \
     CustomUserSerializer
 
@@ -65,7 +67,7 @@ class ClassroomDetailView(generics.RetrieveUpdateDestroyAPIView):
 class AssignmentListCreateView(generics.ListCreateAPIView):
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
-    permission_classes = [IsAdminOrExaminer]
+    permission_classes = []
     authentication_classes = [JWTAuthentication]
 
     def get_queryset(self):
@@ -85,8 +87,33 @@ class AssignmentDetailView(generics.RetrieveUpdateDestroyAPIView):
 class SubmissionListCreateView(generics.ListCreateAPIView):
     queryset = Submission.objects.all()
     serializer_class = SubmissionSerializer
+    permission_classes = [IsAdminOrExaminerOrSubmissionOwner]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        if self.request.user.is_admin or self.request.user.is_staff:
+            return Submission.objects.all()
+        else:
+            return Submission.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.is_admin or user.is_staff:
+            # Allow admins and examiners to specify the user field.
+            serializer.save()
+        else:
+            # For other users, automatically fill the user field with their own ID.
+            assignment = serializer.validated_data['assignment']
+            if assignment.sub_end_date and assignment.sub_end_date < timezone.now():
+                print("expired assignment")
+
+                raise ValidationError({'error': 'Assignment has already expired.'})
+
+            serializer.save(user=user)
 
 
 class SubmissionDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Submission.objects.all()
     serializer_class = SubmissionSerializer
+    permission_classes = [IsAdminOrExaminerOrSubmissionOwner]
+    authentication_classes = [JWTAuthentication]
