148840f186a3e34c2aadaf9608b27bd02567bda0
Nitay Caspi <NitayCaspi12@gmail.com>
2023-09-05 11:13:31 +0300

    add submission permissions

--- assignments/permissions.py
+++ assignments/permissions.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
 
+from assignments.models import Assignment
+
 CustomUser = get_user_model()
 
 
@@ -71,6 +73,33 @@ class IsAdminOrExaminerOrAssignmentOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return obj.course.classrooms.filter(users=request.user).exists()
+        if request.user.is_admin or request.user.is_staff:
+            return True
+        return False
+
+
+class IsAdminOrExaminerOrSubmissionOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        if request.user.is_admin or request.user.is_staff:
+            return True
+        if request.method in permissions.SAFE_METHODS:
+            return request.user.is_authenticated
+        if request.method == 'POST':
+            assignment_id = request.data.get('assignment')
+            if assignment_id:
+                try:
+                    return Assignment.objects.filter(course__classrooms__users=request.user, id=assignment_id).exists()
+                except Assignment.DoesNotExist:
+                    return False
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_admin or request.user.is_staff:
+            return True
+        if request.method in permissions.SAFE_METHODS or request.method in ['PUT', 'PATCH', 'DESTROY']:
+            return obj.user == request.user
         if request.user.is_admin or request.user.is_staff:
             return True
         return False
